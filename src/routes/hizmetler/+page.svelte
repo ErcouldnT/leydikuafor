f217868cfc008be629abf35248101242c7616e5b
Ercode Techoglu <ercode@pm.me>
2023-02-11 01:04:44 +0300

    Fix: Responsive Hizmetler page

--- src/routes/hizmetler/+page.svelte
+++ src/routes/hizmetler/+page.svelte
@@ -5,18 +5,18 @@
 </script>
 
 <main>
-	<div class="grid grid-cols-1 gap-10 p-10">
+	<div class="grid grid-cols-1 gap-10 lg:p-10">
 		{#each hizmetler as hizmet}
 			<div class="grid lg:grid-cols-2 m-auto items-center gap-5">
-				<div class="px-20 {hizmet.fotoYön === 'sağ' && 'lg:order-last'} order-first">
-					<img class="rounded-xl max-w-lg" src={hizmet.foto} alt="Foto" />
+				<div class="lg:px-20 {hizmet.fotoYön === 'sağ' && 'lg:order-last'} order-first">
+					<img class="lg:rounded-xl lg:max-w-lg" src={hizmet.foto} alt="Foto" />
 				</div>
 				<div class="m-auto">
-					<div class="pb-10 flex flex-col justify-center items-center">
+					<div class="mb-5 flex flex-col justify-center items-center">
 						<div class="uppercase font-bold text-xl">{hizmet.kategori}</div>
 						<p class="text-sm">Fiyat Listesi</p>
 					</div>
-					<div class="p-5 border rounded-xl mx-20">
+					<div class="p-5 border rounded-xl m-2 lg:mx-20">
 						{#each hizmet.fiyatlar as hizmet}
 							<div class="grid grid-cols-2 gap-10">
 								<div>{hizmet.ad}</div>
