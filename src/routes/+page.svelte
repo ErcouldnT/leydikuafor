bf6e473266b6d2222d1eb4d0f6cc681352f7e312
Ercode Techoglu <ercode@pm.me>
2023-02-10 16:51:14 +0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Leydi Kuaför | Özel İlgiyi Hak Ediyorsunuz</title>
+	<meta name="description" content="Leydi Kuaför | Özel İlgiyi Hak Ediyorsunuz" />
+</svelte:head>
+
+<div>Leydi Kuaför</div>
