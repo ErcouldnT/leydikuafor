e1a68a9e3467055d6a369738b67bcec2ac0db478
Ercode Techoglu <ercode@pm.me>
2024-06-29 04:56:13 +0300

    qq

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import sombre from '$lib/assets/sombre.jpg?enhanced';
 	import gelin4 from '$lib/assets/gelin4.jpg?enhanced';
 	import ombre from '$lib/assets/ombre.jpg?enhanced';
-	import ombre3 from '$lib/assets/ombre3.jpg?enhanced';
+	import ombre4 from '$lib/assets/ombre4.jpg?enhanced';
 </script>
 
 <main class="grid lg:grid-cols-4 gap-1">
@@ -26,8 +26,8 @@
 	/>
 	<enhanced:img
 		class="h-full w-full aspect-auto hover:opacity-70"
-		src={ombre3}
+		src={ombre4}
 		loading="lazy"
-		alt="ombre3"
+		alt="ombre4"
 	/>
 </main>
